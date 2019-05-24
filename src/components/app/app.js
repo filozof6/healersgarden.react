8d88c2b73596d280f5f8da35d5162033c75d8589
Roman Zahradnik <rzfilozof@gmail.com>
2019-05-24 19:26:32 +0200

    Created base modules

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import logo from '../../assets/images/logo.svg';
-import './App.scss';
+import './app.scss';
+import Menu from '../footer';
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
