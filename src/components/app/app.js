1e9dbee648ae2f60806d9888e219662e62abfdd2
Roman Zahradnik <rzfilozof@gmail.com>
2019-05-25 08:49:09 +0200

    Added redux and router
    changed app from functional to stateful component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,31 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import logo from '../../assets/images/logo.svg';
-import './app.scss';
-import Menu from '../footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from "react";
+import logo from "../../assets/images/logo.svg";
+import "./app.scss";
+import Menu from "../footer";
 
-function App() {
-  return (
-    <div className="App">
-      <Menu />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.asd
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactdasd
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Menu />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.asd
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Reactdasd
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
